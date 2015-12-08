0e4fa3e77e6c88e5a8b5066e28b7a0e6c80eaf4d
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2015-12-08 12:23:14 +0000

    Ensure domains end with an atom. Bumped deps

--- lib/rfc822.rb
+++ lib/rfc822.rb
@@ -2,29 +2,29 @@ require 'timeout'
 require 'uri'
 
 module RFC822
-  
+
   module Patterns
-    
+
     ATOM      = "[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\u00ff]+"
-    
-    QTEXT     = "[^\\x0d\\x22\\x5c\\u0080-\\u00ff]"  
+
+    QTEXT     = "[^\\x0d\\x22\\x5c\\u0080-\\u00ff]"
     QPAIR     = "\\x5c[\\x00-\\x7f]"
     QSTRING   = "\\x22(?:#{QTEXT}|#{QPAIR})*\\x22"
-    
+
     WORD      = "(?:#{ATOM}|#{QSTRING})"
 
     LOCAL_PT  = "#{WORD}(?:\\x2e#{WORD})*"
-    ADDRESS   = "#{LOCAL_PT}\\x40#{URI::REGEXP::PATTERN::HOSTNAME}"    
+    ADDRESS   = "#{LOCAL_PT}\\x40(?:#{URI::REGEXP::PATTERN::HOSTNAME})?#{ATOM}"
 
   end
-  
-  MXRecord = Struct.new(:priority, :host)  
+
+  MXRecord = Struct.new(:priority, :host)
   EMAIL    = /\A#{Patterns::ADDRESS}\z/
-  
+
   @@host_command = '/usr/bin/env host'
-  
+
   class << self
-    
+
     def host_command
       @@host_command
     end
@@ -36,8 +36,8 @@ module RFC822
     def mx_records(address)
       address = address.to_s
       return [] unless address =~ EMAIL
-  
-      Timeout::timeout(2) do 
+
+      Timeout::timeout(2) do
         raw_mx_records(address.split('@').last).map do |priority, host|
           MXRecord.new(priority.to_i, host)
         end
@@ -46,12 +46,12 @@ module RFC822
       []
     end
 
-    def raw_mx_records(domain)      
-      host_mx(domain).scan(/#{Regexp.escape(domain)}[\w ]+?(\d+) (#{URI::REGEXP::PATTERN::HOSTNAME})\./)      
+    def raw_mx_records(domain)
+      host_mx(domain).scan(/#{Regexp.escape(domain)}[\w ]+?(\d+) (#{URI::REGEXP::PATTERN::HOSTNAME})\./)
     end
-    
+
     def host_mx(domain)
-      `#{host_command} -t MX #{domain}`      
+      `#{host_command} -t MX #{domain}`
     end
 
   end
--- spec/rfc822_spec.rb
+++ spec/rfc822_spec.rb
@@ -3,52 +3,54 @@ require File.dirname(__FILE__) + '/helper'
 describe RFC822 do
 
   after do
-    RFC822.host_command = '/usr/bin/env host'    
+    RFC822.host_command = '/usr/bin/env host'
   end
-  
+
   describe 'email pattern' do
-    
+
     it 'should match RFC822 compliant addresses' do
-      "user@home".should match(RFC822::EMAIL)
-      "user@home.com".should match(RFC822::EMAIL)
-      "user@dashed-home.info".should match(RFC822::EMAIL)
-      "user@separated.home.info".should match(RFC822::EMAIL)
-      "dashed-user@home.com".should match(RFC822::EMAIL)            
-      "underscored_user@home.com".should match(RFC822::EMAIL)            
-      "separated.user@home.com".should match(RFC822::EMAIL)            
-      "slashed/user@home.com".should match(RFC822::EMAIL)            
-      "plussed+user@home.com".should match(RFC822::EMAIL)            
-      "{wrapped}user@home.com".should match(RFC822::EMAIL)            
-      "end-dashed-@home.com".should match(RFC822::EMAIL)            
+      expect("user@x").to match(RFC822::EMAIL)
+      expect("user@home").to match(RFC822::EMAIL)
+      expect("user@home.com").to match(RFC822::EMAIL)
+      expect("user@dashed-home.info").to match(RFC822::EMAIL)
+      expect("user@separated.home.info").to match(RFC822::EMAIL)
+      expect("dashed-user@home.com").to match(RFC822::EMAIL)
+      expect("underscored_user@home.com").to match(RFC822::EMAIL)
+      expect("separated.user@home.com").to match(RFC822::EMAIL)
+      expect("slashed/user@home.com").to match(RFC822::EMAIL)
+      expect("plussed+user@home.com").to match(RFC822::EMAIL)
+      expect("{wrapped}user@home.com").to match(RFC822::EMAIL)
+      expect("end-dashed-@home.com").to match(RFC822::EMAIL)
     end
 
     it 'should not match non-RFC822 compliant addresses' do
-      "user-home".should_not match(RFC822::EMAIL)
-      "@home.com".should_not match(RFC822::EMAIL)
-      "user@".should_not match(RFC822::EMAIL)
-      "user@underscored_home.org".should_not match(RFC822::EMAIL)
-      "user@just,wrong.org".should_not match(RFC822::EMAIL)
-      "user@just/wrong.org".should_not match(RFC822::EMAIL)
-      "user@just wrong.org".should_not match(RFC822::EMAIL)
-      "wrong,user@home.com".should_not match(RFC822::EMAIL)            
-      "wrong-user.@home.com".should_not match(RFC822::EMAIL)            
-      "wrong@user@home.com".should_not match(RFC822::EMAIL)            
-      "wrong user@home.com".should_not match(RFC822::EMAIL)            
-      "wrong(user)@home.com".should_not match(RFC822::EMAIL)            
-    end    
-    
+      expect("user-home").not_to match(RFC822::EMAIL)
+      expect("@home.com").not_to match(RFC822::EMAIL)
+      expect("user@").not_to match(RFC822::EMAIL)
+      expect("user@trailing.period.").not_to match(RFC822::EMAIL)
+      expect("user@underscored_home.org").not_to match(RFC822::EMAIL)
+      expect("user@just,wrong.org").not_to match(RFC822::EMAIL)
+      expect("user@just/wrong.org").not_to match(RFC822::EMAIL)
+      expect("user@just wrong.org").not_to match(RFC822::EMAIL)
+      expect("wrong,user@home.com").not_to match(RFC822::EMAIL)
+      expect("wrong-user.@home.com").not_to match(RFC822::EMAIL)
+      expect("wrong@user@home.com").not_to match(RFC822::EMAIL)
+      expect("wrong user@home.com").not_to match(RFC822::EMAIL)
+      expect("wrong(user)@home.com").not_to match(RFC822::EMAIL)
+    end
+
   end
-  
+
   it 'should allow to change the host command' do
-    RFC822.host_command.should == '/usr/bin/env host'
+    expect(RFC822.host_command).to eq('/usr/bin/env host')
     RFC822.host_command = '/opt/bin host'
-    RFC822.host_command.should == '/opt/bin host'     
+    expect(RFC822.host_command).to eq('/opt/bin host')
   end
-  
+
   describe 'MX record check' do
 
     before do
-      RFC822.stub(:host_mx).and_return(%Q(
+      allow(RFC822).to receive(:host_mx).and_return(%Q(
 hotmail.com mail is handled by 5 mx3.hotmail.com.
 hotmail.com mail is handled by 5 mx4.hotmail.com.
 hotmail.com mail is handled by 5 mx1.hotmail.com.
@@ -57,24 +59,24 @@ hotmail.com mail is handled by 5 mx2.hotmail.com.
     end
 
     it 'should return raw MX records fo a domain' do
-      RFC822.raw_mx_records('hotmail.com').should == [
-        ["5", "mx3.hotmail.com"], 
-        ["5", "mx4.hotmail.com"], 
-        ["5", "mx1.hotmail.com"], 
-        ["5", "mx2.hotmail.com"]
-      ]
+      expect(RFC822.raw_mx_records('hotmail.com')).to eq([
+        ["5", "mx3.hotmail.com"],
+        ["5", "mx4.hotmail.com"],
+        ["5", "mx1.hotmail.com"],
+        ["5", "mx2.hotmail.com"],
+      ])
     end
 
     it 'should return prioritised MX records of an address' do
-      RFC822.mx_records('user@hotmail.com').should have(4).items
+      expect(RFC822.mx_records('user@hotmail.com').size).to eq(4)
       record = RFC822.mx_records('user@hotmail.com').first
-      record.priority.should == 5
-      record.host.should == "mx3.hotmail.com"
+      expect(record.priority).to eq(5)
+      expect(record.host).to eq("mx3.hotmail.com")
     end
 
     it 'should return an empty result if address is invalid' do
-      RFC822.mx_records('user@hotmail@com').should == []
+      expect(RFC822.mx_records('user@hotmail@com')).to eq([])
     end
-   
+
   end
 end
